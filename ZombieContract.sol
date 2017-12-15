aa633c850629536289521e4c316d5ca84017ce42
Keith <keithtmccartney@hotmail.com>
2017-12-15 21:13:18 +0000

    UINT Lesson unlocked!

--- ZombieContract.sol
+++ ZombieContract.sol
@@ -1,5 +1,12 @@
-pragma solidity ^0.4.19; //1. Enter solidity version here
+//1. Enter solidity version here
+
+pragma solidity ^0.4.19;
 
 //2. Create contract here
 contract ZombieFactory {
+    //3. Start here
+
+    //The uint data type is an unsigned integer, meaning its value must be non-negative. There's also an int data type for signed integers
+    //This will be stored permanently in the blockchain
+    uint dnaDigits = 16;
 }
